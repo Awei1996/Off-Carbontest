354b30e70201c906029cb4d2aa192393cdfca9eb
Awei1996 <bill010809@etech.ncyu.edu.tw>
2021-07-11 22:51:39 +0800

    Update littleMenu.js

--- littleMenu.js
+++ littleMenu.js
@@ -9,7 +9,7 @@ var MenuBtnControl = document.getElementsByClassName('MenuBtn')[0];
 
 var MenuValue = 0 ;
 
-var ShowRange = window.matchMedia("(max-device-width: 960px)") 
+var ShowRange = window.matchMedia("(max-width: 960px)") 
 
 
 window.matchMedia = function(){MenuShow();};
