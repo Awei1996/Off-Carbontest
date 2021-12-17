38bac3fdb0f6698db4388ee5f4094eec6ca822b8
Awei1996 <bill010809@etech.ncyu.edu.tw>
2021-12-17 10:52:48 +0800

    Update flip-card.js

--- flip-card.js
+++ flip-card.js
@@ -9,22 +9,8 @@ function flip(event){
 		}
 	}
 	
-	if (element.className === "oilcard") {
-    if(element.style.webkitTransform == "rotateY(180deg)") {
-      element.style.webkitTransform = "rotateY(0deg)";
-    }
-    else {
-      element.style.webkitTransform = "rotateY(180deg)";
-    }
-  }
-	if (element.className === "lifecard") {
-    if(element.style.webkitTransform == "rotateY(180deg)") {
-      element.style.webkitTransform = "rotateY(0deg)";
-    }
-    else {
-      element.style.webkitTransform = "rotateY(180deg)";
-    }
-  }
+	
+  
 }// JavaScript Document
 
 $(document).ready(function () {
