ac7ffae24176c4cbbccbd61ef3a2c2f5c0b819f1
Awei1996 <bill010809@etech.ncyu.edu.tw>
2022-01-04 15:23:31 +0800

    Update DeviceCheck.js

--- 網頁/AttackPage/DeviceCheck.js
+++ 網頁/AttackPage/DeviceCheck.js
@@ -2,9 +2,9 @@
 function isMobileDevice(){
     var mobileDevice = ['Android', 'webOS', 'iPhone', 'iPad', 'iPod', 'BlackBerry', 'Windows Phone'];
     var isMobileDevice = false ;
-    for(var i=0;i<mobileDevice.length;i++){
+    for(var i=0; i<mobileDevice.length; i++){
         if(navigator.userAgent.match(mobileDevice[i])){
-            isMobileDevice= true ;
+            isMobileDevice = true ;
         }
 	
 	}
