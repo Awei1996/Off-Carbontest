50d90a2549a45fccfc2de99c36eee57c2fe349e0
Awei1996 <bill010809@etech.ncyu.edu.tw>
2022-01-04 15:22:09 +0800

    NEW Mobile test

---
+++ 網頁/AttackPage/DeviceCheck.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+function isMobileDevice(){
+    var mobileDevice = ['Android', 'webOS', 'iPhone', 'iPad', 'iPod', 'BlackBerry', 'Windows Phone'];
+    var isMobileDevice = false ;
+    for(var i=0;i<mobileDevice.length;i++){
+        if(navigator.userAgent.match(mobileDevice[i])){
+            isMobileDevice= true ;
+        }
+	
+	}
+    return isMobileDevice ;
+}
