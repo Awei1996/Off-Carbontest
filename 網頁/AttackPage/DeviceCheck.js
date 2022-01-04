e69dea3ace204dff0760276a7d220b2c93989d4b
Awei1996 <bill010809@etech.ncyu.edu.tw>
2022-01-04 16:58:07 +0800

    Update DeviceCheck.js

--- 網頁/AttackPage/DeviceCheck.js
+++ 網頁/AttackPage/DeviceCheck.js
@@ -2,6 +2,3 @@
  if(navigator.userAgent.match(/Android|iPhone|iPad/i)) {
      window.location = '../AttackPage/AttackPage.m.html';
  }
-else{
-		window.location = '../AttackPage/AttackPage.html';
-	}
